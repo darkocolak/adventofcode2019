9ac19ccdf409f4f60bdddd95baf5535bfd0f522c
Darko Colak <darko.colak@devtechgroup.com>
2019-12-06 17:20:08 +0100

    day3 wip

---
+++ csharp/day3/Program-badsolution.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+
+namespace day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            var lines1 = input[0];
+            var lines2 = input[1];
+            //var lines1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
+            //var lines2 = "U62,R66,U55,R34,D71,R55,D58,R83";
+            //var lines1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51";
+            //var lines2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+
+            var startX = 15000;
+            var startY = 15000;
+
+            var board1 = new int[40000, 40000];
+            var board2 = new int[40000, 40000];
+            DrawLine(board1,startX, startY, lines1);
+            DrawLine(board2, startX, startY, lines2);
+            //PrintBoard(board1);
+            //PrintBoard(board2);
+
+            
+            var resultingBoard = MergeBoards(board1, board2);
+            resultingBoard[startX, startY] = 1;
+            //PrintBoard(resultingBoard);
+            var distanceFromStart = CalculateShortestDistance(resultingBoard, startX, startY);//1431 - Works but is a terribly slow and memory hungry solution
+        }
+
+        private static int CalculateShortestDistance(int[,] board, int startX, int startY)
+        {
+            var minDistance = board.GetLength(0) * 2;
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if(board[i,j] ==2)
+                    {
+                        var distanceFromStart = Math.Abs(startX - i) + Math.Abs(startY-j);
+                        if (distanceFromStart < minDistance) minDistance = distanceFromStart;
+                    }                    
+                }
+            }
+            return minDistance;
+        }
+
+        private static int[,] MergeBoards(int[,] board1, int[,] board2)
+        {
+            var resultingBoard = new int[board1.GetLength(0), board1.GetLength(1)];
+
+            for (int i = 0; i < board1.GetLength(0); i++)
+            {
+                for (int j = 0; j < board1.GetLength(1); j++)
+                {
+                    resultingBoard[i, j] = board1[i, j] + board2[i, j];
+                }
+            }
+            return resultingBoard;
+        }
+
+        private static void PrintBoard(int[,] board)
+        {
+            for(int i = 0; i< board.GetLength(0); i++)
+            {
+                for(int j = 0; j < board.GetLength(1); j++)
+                {
+                    Console.Write(board[i, j]);
+                }
+                Console.Write("\n");
+            }
+        }
+
+        private static void DrawLine(int[,] board, int startX, int startY, string lines1)
+        {
+            var currentPositionX = startX;
+            var currentPositionY = startY;
+            board[startX, startY] = 1;
+            foreach (var line in lines1.Split(','))
+            {
+                var direction = line[0];
+                var steps = int.Parse(line.Substring(1));
+
+                for (int i = 1; i < steps; i++)
+                {
+                    if (direction == 'L')
+                    {
+                        currentPositionY--;
+                        board[currentPositionX, currentPositionY] = 1;
+                    }
+                    else if (direction == 'R')
+                    {
+                        currentPositionY++;
+                        board[currentPositionX, currentPositionY] = 1;                        
+                    }
+                    else if (direction == 'U')
+                    {
+                        currentPositionX--;
+                        board[currentPositionX, currentPositionY] = 1;
+                        
+                    }
+                    else if (direction == 'D')
+                    {
+                        currentPositionX++;
+                        board[currentPositionX, currentPositionY] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+}
---
+++ csharp/day3/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day3
+{
+    public class Line
+    {
+        public Point Start { get; set; }
+        public Point End { get; set; }
+        public int Length { get; set; }
+        public bool IsUpright { get; set; }
+    }
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            var commandsOne = input[0];
+            var commandsTwo = input[1];
+
+            var lineOne = CommandsToLines(commandsOne);
+            var lineTwo = CommandsToLines(commandsTwo);
+            var intersections = GetIntersections(lineOne, lineTwo);
+
+        }
+
+        private static List<Point> GetIntersections(List<Line> lineOne, List<Line> lineTwo)
+        {
+            var intersectionPoints = new List<Point>();
+            foreach (var partOfLineOne in lineOne)
+            {
+                foreach (var partOfLineTwo in lineTwo)
+                {
+                    var intersectionPoint = GetIntersectionPoint(partOfLineOne, partOfLineTwo);
+                    if (intersectionPoint != null)
+                    {
+                        intersectionPoints.Add(intersectionPoint);
+                    }
+                }
+            }
+            return intersectionPoints;
+        }
+
+        private static Point GetIntersectionPoint(Line lineOne, Line lineTwo)
+        {
+            if (lineOne.IsUpright ^ lineTwo.IsUpright) return null;
+            if (lineOne.IsUpright)
+            {
+                var max1x = Math.Max(lineOne.Start.X, lineOne.End.X);
+                var min1x = Math.Min(lineOne.Start.X, lineOne.End.X);
+                var max2x = Math.Max(lineTwo.Start.X, lineTwo.End.X);
+                var min2x = Math.Min(lineTwo.Start.X, lineTwo.End.X);
+                var max1y = Math.Max(lineOne.Start.Y, lineOne.End.Y);
+                var min1y = Math.Min(lineOne.Start.Y, lineOne.End.Y);
+                var max2y = Math.Max(lineTwo.Start.Y, lineTwo.End.Y);
+                var min2y = Math.Min(lineTwo.Start.Y, lineTwo.End.Y);
+
+
+                if (lineTwo.Start.X <= lineOne.Start.X && lineOne.Start.X <= lineTwo.End.X &&
+                    lineOne.Start.Y <= lineOne.Start.Y && lineOne.Ene.Y <= lineOne.Start.Y)
+                {
+                    return new Point(lineOne.Start.X, lineTwo.Start.Y);
+                }
+            }
+            else
+            {
+                //line 2 is upright
+                if(lineOne.Start.X < )
+            }
+            return null;
+        }
+
+        private static List<Line> CommandsToLines(string commandsTwo)
+        {
+            var currentX = 0;
+            var currentY = 0;
+            var lines = new List<Line>();
+
+            foreach (var line in commandsTwo.Split(','))
+            {
+                var direction = line[0];
+                var lineLen = int.Parse(line.Substring(1));
+                var newLine = new Line
+                {
+                    Start = new Point(currentX, currentY),
+                    End = new Point(currentX, currentY),
+                    Length = lineLen
+                };
+                if (direction == 'L')
+                {
+                    currentX -= lineLen;
+                    newLine.End.X = currentX;
+                    newLine.IsUpright = false;
+                }
+                else if (direction == 'R')
+                {
+                    currentX += lineLen;
+                    newLine.End.X = currentX;
+                    newLine.IsUpright = false;
+                }
+                else if (direction == 'U')
+                {
+                    currentY += lineLen;
+                    newLine.End.Y = currentY;
+                    newLine.IsUpright = true;
+                }
+                else if (direction == 'D')
+                {
+                    currentY -= lineLen;
+                    newLine.End.Y = currentY;
+                    newLine.IsUpright = true;
+                }
+            }
+            return lines;
+        }
+
+
+    }
+
+
+
+
+}
